606bc42ad68aa4307dea73272bf7e6e71da2c6ac
Jaephil <kws00100@naver.com>
2017-11-23 04:42:37 -0800

    info2

--- include/info.h
+++ include/info.h
@@ -2,5 +2,9 @@
 #define __INFO_H_
 
 void ch_terminal();
+void ch_time();
 
 char *terminal[5] = { "Incheon","DongSeoul","Gimpo_Airport","Bucheon","Yongin"};
+
+char *time[10] = {"7:00","8:00","9:00","10:00","11:00","12:00","14:00","16:00","18:00","20:00","22:00" };
+
