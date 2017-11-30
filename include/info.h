d4ebbe6fc68dec51a2f3206fc524fbefac603930
Jaephil <kws00100@naver.com>
2017-11-29 23:38:51 -0800

    info3

--- include/info.h
+++ include/info.h
@@ -8,3 +8,5 @@ char *terminal[5] = { "Incheon","DongSeoul","Gimpo_Airport","Bucheon","Yongin"};
 
 char *time[10] = {"7:00","8:00","9:00","10:00","11:00","12:00","14:00","16:00","18:00","20:00","22:00" };
 
+char *credit[4] = {"shinhan card", "kb card", "nonghyup card","woori card","hyundai card"};
+
