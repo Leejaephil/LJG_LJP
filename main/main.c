74455b9f61507a946c467d9220baa00e1c215b06
Jaephil <kws00100@naver.com>
2017-12-05 05:26:24 -0800

    main

--- main/main.c
+++ main/main.c
@@ -1,9 +1,12 @@
-#include<stdio.h>
+#include "../include/info.h"
 
 
 int main()
 {
-    
+    ch_payment();
+    ch_date(&year, &month, &day);
+    ch_terminal(terminal,&ch_d);
+    ch_time(time_table,&ch_t);
 
     ch_seat(st_num);//select seat
     view_res();//view Reservation data
