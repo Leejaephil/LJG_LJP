2107a8bd503d98426c72fac5e6ed4cc28c9c3798
Jingu Lee <fjqm1211@naver.com>
2017-12-05 18:09:03 +0900

    add st_num to info.h

--- include/info.h
+++ include/info.h
@@ -10,3 +10,4 @@ char *time[10] = {"7:00","8:00","9:00","10:00","11:00","12:00","14:00","16:00","
 
 char *credit[4] = {"shinhan card", "kb card", "nonghyup card","woori card","hyundai card"};
 
+int st_num;
