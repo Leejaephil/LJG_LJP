f07dba49fb8ba4af42b4eb29404948fff0eddc36
Jaephil <kws00100@naver.com>
2017-12-05 03:51:37 -0800

    payment2_1205

---
+++ data/payment2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+void ch_payment()
+{
+	int met;
+	int pay;
+
+	printf("Please select payment method\n");
+	printf("1.On-site payment\n 2.Mobile payment\n");
+	scanf("%d",&met);
+
+	if (met == 1)
+	{
+		printf("Payment schedule has been completed.\n");
+	}
+	else 
+	{
+		printf("Please select payment method.\n");
+		printf("1.Account transfer\n 2.Credit card\n");
+		scanf("%d",&pay);
+
+			if (pay == 1)
+			{
+				printf("Please send us to 1111-2222-3333\n");
+			}
+			else 
+			{
+				printf("Please select a card\n");
+				printf("Shinhan card, kb card, nonghyup card, woori card\n");
+
+			}
+	}
+}
+
+int main(void)
+{
+	ch_payment();
+	return 0;
+}
