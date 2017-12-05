4b9b48e4c81a0b28e9f67eeb213c73ee9c67dce3
Jaephil <kws00100@naver.com>
2017-12-05 04:25:47 -0800

    payment_1205

--- data/payment.c
+++ data/payment.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 
 
-void ch_payment(char *credit,char *ch)
+void ch_payment()
 {
 	int met;
 	int pay;
 
 	printf("Please select payment method\n");
 	printf("1.On-site payment\n 2.Mobile payment\n");
-	scanf("%d",sel);
+	scanf("%d",&met);
 
 	if (met == 1)
 	{
 		printf("Payment schedule has been completed.\n");
 	}
-	else (met == 2)
+	else 
 	{
 		printf("Please select payment method.\n");
 		printf("1.Account transfer\n 2.Credit card\n");
-		scanf("%d",pay);
+		scanf("%d",&pay);
 
 			if (pay == 1)
 			{
 				printf("Please send us to 1111-2222-3333\n");
 			}
-			else (pay == 2)
+			else 
 			{
 				printf("Please select a card\n");
-				for (int i=0; i<5 i++)
-					pritnf("%d. %s\n, i+1, credit[i]);
-					scanf("%c",ch);
-
-					printf("You've chosen %s!\n",credit[(*ch)-1]);
+				printf("Shinhan card, kb card, nonghyup card, woori card\n");
 
 			}
 	}
