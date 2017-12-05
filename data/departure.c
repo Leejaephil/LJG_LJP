9107f1ad2e52162e7c4818ec7b0e22782a91d7a0
Jaephil <kws00100@naver.com>
2017-12-05 03:13:34 -0800

    departure_1205

--- data/departure.c
+++ data/departure.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-void ch_terminal(char *terminal,char *ch)
+void ch_terminal(char *terminal,char *ch_d)
 {
 
 	printf("EsE bus reservation sysyem\n");
@@ -8,24 +8,25 @@ void ch_terminal(char *terminal,char *ch)
 	printf("Please choose a departure terminal!\n");
 
 	for (int i=0; i<5; i++)
-		printf("%d) %s\n", i+1, terminal[i]);
+		printf("%d) %s\n", i+1, *terminal[i]);
 
 
 	while(1)
 	{
 		printf("write number  of terminal\n");
-		scanf("%c", ch);
+		scanf("%d", ch_d);
 
-		if(*ch -gt 5 || *ch -lt 0)
+		if(*ch > 5 || *ch < 0)
 		{
 			printf("Please write in 1 ~ 5\n");
 			printf("Let me explain once more\n");
 				for (i=0; i<5; i++)
 					printf("%d. %s\n", i+1, terminal[i]);
+			continue;
 		}
 		else
 		{
-			printf("You've chosen %s!\n,",terminal[(*ch)-1]);
+			printf("You've chosen %s!\n,",terminal[(*ch_d)-1]);
                         printf("Please select the Arrival terminal next time.\n");
 			break;
 		}
